3d80ae6293f055fe56c453549c132dca741108d9
umidovalimardon06 <umidovalimardon06@gmail.com>
2025-09-20 15:47:55 +0500

    adds new line to Main.java (new-1)

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.umidov.lesson11;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GIT-HUB");
-
+        System.out.println("new-1");
 
     }
 }
