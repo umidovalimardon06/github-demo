dfdb370e6495f68a2e163585511a2e30ec67473c
Umidov Alimardon <umidovalimardon06@gmail.com>
2025-09-20 15:56:31 +0500

    Update Main.java
    adds new-2 from git-hub

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GIT-HUB");
         System.out.println("new-1");
+        System.out.println("new-2(inside-github)");
 
     }
 }
